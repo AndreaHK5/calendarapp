290c6c34029d66591c2fc185b7cbab592b064342
AndreaHK5 <cremese@gmail.com>
2016-01-14 17:10:28 -0800

    trip create initialized

--- client/templates/_partials/nav.js
+++ client/templates/_partials/nav.js
@@ -3,7 +3,6 @@ Template.nav.helpers({
     return Router.current().route.path() == "/";
   },
   inAddTrip : function () {
-    console.log(Router.current().route.path());
     return Router.current().route.path() == "/create";
   }
 })
--- client/templates/trips/create.js
+++ client/templates/trips/create.js
@@ -1,7 +1,11 @@
+Template.tripsCreate.onRendered(function (){
+    Session.set("startDate", undefined);
+    Session.set("endDate", undefined);
+})
+
 Template.tripsCreate.helpers({
   datesMissing : function () {
-    return !Session.get("startDate")
-      || !Session.get("endDate");
+    return !Session.get("startDate") || !Session.get("endDate");
   },
   getStartDate : function () {
     return Session.get("startDate");
