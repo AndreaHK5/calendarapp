9c58d4a11f1cf0b2889ee50d10ef7a53f2a3aca8
AndreaHK5 <cremese@gmail.com>
2016-01-19 13:55:33 -0800

    navbar transitioned to semantic

--- client/templates/_partials/nav.js
+++ client/templates/_partials/nav.js
@@ -5,4 +5,17 @@ Template.nav.helpers({
   inAddTrip : function () {
     return Router.current().route.path() == "/create";
   }
+});
+
+Template.nav.onRendered(function(){
+  // open the dropdown
+  $('.right.menu.open').on("click",function(e){
+    e.preventDefault();
+    $('.ui.vertical.menu').toggle();
+  });
+  $('.ui.dropdown').dropdown();
+  
+  $(".vertical a.item").on("click",function(e){
+    $('.ui.vertical.menu').toggle();
+  });
 })
