3608c644fd690044b6ee42f1561f72626d01eee2
Andrea <cremese@gmail.com>
2016-01-27 12:05:52 -0800

    hide arrow on events as scrolling will differ

--- client/templates/_partials/trips_create_nav.js
+++ client/templates/_partials/trips_create_nav.js
@@ -2,3 +2,10 @@ Template.tripsCreateNav.onRendered(function(){
   var siteNavbarHeight = $("#site-navbar").height();
   $('#weekday-navbar').css("margin-top", siteNavbarHeight);
 });
+
+Template.tripsCreateNav.helpers({
+  showArrows : function () {
+    // arrow hidden on events as not working
+    return Router.current().route.getName() != "eventsIndex";    
+  }
+})
